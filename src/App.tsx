e463a67e377fe1f801e6dc0e06e9e914607c55a6
Larissa <larissa.smata@hotmail.com>
2025-04-11 15:54:13 -0300

    Implementação de autenticação, crud e formulário.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@
     import './App.css'
     import Footer from './components/footer/Footer'
     import Navbar from './components/navbar/Navbar'
+    import { AuthProvider } from './contexts/AuthContext'
     import Home from './pages/home/Home'
     import Login from './pages/login/Login'
     import Cadastro from './pages/cadastro/Cadastro'
+import ListaTemas from './components/temas/listatemas/ListaTemas'
+import FormTema from './components/temas/formtema/FormTema'
+import DeletarTema from './components/temas/deletartema/DeletarTema'
+import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
+import FormPostagem from './components/postagens/formpostagem/Formpostagem'
+import DeletarPostagem from './components/temas/deletarpostagem/DeletarPostagem'
 
     function App() {
     return (
+
         <>
+        <AuthProvider>
             <BrowserRouter>
             <Navbar />
             <div className="min-h-[80vh]">
@@ -17,10 +26,19 @@
                 <Route path="/home" element={<Home />} />
                 <Route path="/cadastro" element={<Cadastro />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/temas" element={<ListaTemas />} />
+                <Route path="/cadastrartema" element={<FormTema />} />
+                <Route path="/editartema/:id" element={<FormTema />} />
+                <Route path="/deletartema/:id" element={<DeletarTema />} />
+                <Route path="/postagens" element={<ListaPostagens />} />
+                <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+                <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+                <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
                 </Routes>
             </div>
             <Footer />
             </BrowserRouter>
+        </AuthProvider>
         </>
     )
     }
